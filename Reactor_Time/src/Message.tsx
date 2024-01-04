4b228dea61eb92635ad43cbbc2952f5c80587a8d
red2922 <inthavongsaj@lopers.unk.edu>
2024-01-04 17:32:28 -0600

    Made My First React Component
    Learned about React and PascalCasing

---
+++ Reactor_Time/src/Message.tsx
@@ -0,0 +1,12 @@
+// PascalCasing- everyword is uppercased
+
+function Message() {
+    //Called JSX: Javascript XML
+    const name = 'Rose';
+
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello World</h1>
+}
+
+export default Message;
