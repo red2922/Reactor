c61e9f859f96319ffb55503e396aa37997fe7bc7
red2922 <inthavongsaj@lopers.unk.edu>
2024-01-05 16:46:05 -0600

    Added Personal Flair + Created Event Handler

--- Reactor_Time/src/components/ListGroup.tsx
+++ Reactor_Time/src/components/ListGroup.tsx
@@ -1,12 +1,26 @@
+import { MouseEvent } from "react";
 function ListGroup() {
+  let names = ["Rose", "Red", "Lily", "Violet", "Rikki", "Eclipse", "Zero"];
+
+  //Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  //Both names.length conditional functiosn do the same thing within the code
+  //Works as true and true == true
+  //Otherwise it is false and true which is equal to false
   return (
-    <ul className="list-group">
-      <li className="list-group-item">Rose</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>Character Names</h1>
+      {names.length === 0 ? <p>No Character Found</p> : null}
+      {names.length === 0 && <p>No Character Found</p>}
+      <ul className="list-group">
+        {names.map((item) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
