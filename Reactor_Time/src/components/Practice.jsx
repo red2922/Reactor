e9fbe94282837075fed7907a1b0fa0695333ace4
red2922 <inthavongsaj@lopers.unk.edu>
2024-01-06 03:39:59 -0600

    Started on FreeCodeCamp React

---
+++ Reactor_Time/src/components/Practice.jsx
@@ -0,0 +1,21 @@
+const JSX = <h1>Hello JSX!</h1>;
+
+const JSX_2 = (
+  <div>
+    <h1></h1>
+    <p></p>
+    <ul>
+      <li>Rose</li>
+      <li>Red</li>
+      <li>Lily</li>
+    </ul>
+  </div>
+);
+
+const Comment = (
+  <div>
+    {/*This is a comment */}
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+  </div>
+);
