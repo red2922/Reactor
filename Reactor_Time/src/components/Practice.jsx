cb1be9dd5ae7bb4348025803ffcba82fc5ef8fdc
red2922 <inthavongsaj@lopers.unk.edu>
2024-01-08 17:03:21 -0600

    Nested Components

--- Reactor_Time/src/components/Practice.jsx
+++ Reactor_Time/src/components/Practice.jsx
@@ -52,6 +52,7 @@ const ChildComponent = () => {
   );
 };
 
+//Parent
 const ParentComponent = () => {
   return (
     <div>
@@ -60,3 +61,45 @@ const ParentComponent = () => {
     </div>
   );
 };
+
+const TypesOfFruit = () => {
+  return (
+    <div>
+      <h2>Fruits:</h2>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  );
+};
+
+//Nesting Components
+const Fruits = () => {
+  return (
+    <div>
+      {/* Change code below this line */}
+      <TypesOfFruit />
+      {/* Change code above this line */}
+    </div>
+  );
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
