cafa1a5c1689fa6bcdab6f8b02582b3c19bd8b58
red2922 <inthavongsaj@lopers.unk.edu>
2024-01-09 16:12:25 -0600

    Added Button that makes an alert visible

--- Reactor_Time/src/App.tsx
+++ Reactor_Time/src/App.tsx
@@ -1,22 +1,26 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
   let names = ["Rose", "Red", "Lily", "Violet", "Rikki", "Eclipse", "Zero"];
-
+  const [alertVisible, setAlertVisibility] = useState(false);
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+
   return (
     <div>
-      <Alert>Hello World</Alert>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
+      )}
       <ListGroup
         items={names}
         heading="Character Names"
         onSelectItem={handleSelectItem}
       />
-      <Button onClick={() => {}}>My First Button</Button>
+      <Button onClick={() => setAlertVisibility(true)}>My First Button</Button>
     </div>
   );
 }
