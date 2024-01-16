e7a1c6f7183d8cc2bf5d34f32042f17f43389d0a
red2922 <inthavongsaj@lopers.unk.edu>
2024-01-15 22:23:04 -0600

    Fixed/Updated NavBar

--- Reactor_Time/src/components/NavBar.tsx
+++ Reactor_Time/src/components/NavBar.tsx
@@ -1,27 +1,25 @@
 const NavBar = () => {
   return (
-    <ul className="nav">
-      <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="index.html">
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Link
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">
-          Link
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" aria-disabled="true">
-          Disabled
-        </a>
-      </li>
-    </ul>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Home
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+    </>
   );
 };
 
