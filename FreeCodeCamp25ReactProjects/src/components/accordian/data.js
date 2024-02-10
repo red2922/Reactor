05c50ed92e490c01bc2d29f2061741c0701450e4
red2922 <inthavongsaj@lopers.unk.edu>
2024-02-09 18:36:59 -0600

    Started on 25 different react projects

---
+++ FreeCodeCamp25ReactProjects/src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: "1",
+    question: "What are accordian components?",
+    answer:
+      "Accordian components are user interface elements used for organization",
+  },
+  {
+    id: "2",
+    question: "What are they used for?",
+    answer:
+      "THey are commonly employed in various contexts, including FAQs, and descriptions",
+  },
+  {
+    id: "3",
+    question: "Accoordion as a musical instrument",
+    answer: "The accordion is a musical instrument with a keyboard and bellows",
+  },
+  {
+    id: "4",
+    question: "Can I create an accordion component with a different framework?",
+    answer: "It is very possible to create accordion with different libraries",
+  },
+];
