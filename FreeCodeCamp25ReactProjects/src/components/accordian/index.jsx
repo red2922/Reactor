1bc872ebd28c06ed452d329b068708a910bd7acd
red2922 <inthavongsaj@lopers.unk.edu>
2024-03-22 23:51:31 -0500

    bonked it D:

--- FreeCodeCamp25ReactProjects/src/components/accordian/index.jsx
+++ FreeCodeCamp25ReactProjects/src/components/accordian/index.jsx
@@ -8,25 +8,46 @@ import "./styles.css";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+  const [enableMulti, setMultiSelect] = useState(false);
+  const [getMulti, setMulti] = useState([]);
 
   const handleSingleSelection = (getCurrentId) => {
     setSelected(getCurrentId === selected ? null : getCurrentId);
   };
 
+  const handleMultiSelection = (getCurrentId) => {
+    let copyMulti = [...getMulti];
+    const findIndex = copyMulti.indexOf(getCurrentId);
+
+    if (findIndex === -1) copyMulti.push(getCurrentId);
+    else copyMulti.splice(getCurrentId, 1);
+
+    setMulti(copyMulti);
+  };
+
   return (
     <div className="wrapper">
+      {/* !enableMulti is a great + quick way to toggle something */}
+      <button onClick={() => setMultiSelect(!enableMulti)}>
+        Enable Multi Selection
+      </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
               <div
-                onClick={() => handleSingleSelection(dataItem.id)}
+                onClick={
+                  enableMulti
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ? (
+              {selected === dataItem.id ||
+              getMulti.indexOf(dataItem.id) !== -1 ? (
                 <div className="content">{dataItem.answer}</div>
               ) : null}
             </div>
