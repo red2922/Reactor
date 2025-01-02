bdc191030f2621511f585b5ff1bb61dda486a6dc
Jake Inthavongsa <jinthavongsa1@gmail.com>
2025-01-01 23:58:41 -0600

    Bugged

--- react-challenges/src/components/toDoList.tsx
+++ react-challenges/src/components/toDoList.tsx
@@ -2,29 +2,42 @@ import { useState } from "react";
 
 function ToDoList() {
   const [cityList, setCityList] = useState<string[]>([]);
+  const [userInput, setUserInput] = useState("");
   //By adding the ! it tells typescript that it does indeed exists not matter what.
   const city = document.getElementById("cityInput")!;
 
   const addCities = () => {
     var cityInput: string = (city as HTMLInputElement).value;
 
-    if (cityInput !== null || cityInput !== "") {
+    if (userInput !== null || userInput !== "") {
       var localList = cityList;
-      localList.push(cityInput);
-
+      localList.push(userInput);
+      cityInput = "";
       setCityList(localList);
     }
-    console.log(cityList);
   };
 
   return (
     <>
       <form>
-        <input id="cityInput" type="text" placeholder="Add a City"></input>
+        <input
+          id="cityInput"
+          type="text"
+          placeholder="Add a City"
+          onInput={(e) => {
+            setUserInput((e.target as HTMLTextAreaElement).value);
+          }}
+        ></input>
         <button type="button" onClick={addCities}>
           Add
         </button>
       </form>
+      <ul>
+        {cityList.length !== 0 &&
+          cityList.map((cityName: string) => {
+            return <li>{cityName}</li>;
+          })}
+      </ul>
     </>
   );
 }
